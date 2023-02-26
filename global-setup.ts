deb1148564e58e4a6291d511313167f57817d127
Gioele Antoci <gioele@sportlogiq.com>
2023-02-26 00:49:07 -0500

    Typo

--- global-setup.ts
+++ global-setup.ts
@@ -28,7 +28,7 @@ async function globalSetup() {
             index: b.rowIndex
         }));
 
-    console.log("Writing Sheet rows to file. New Booking count:", bookings.length');
+    console.log("Writing Sheet rows to file. New Booking count:", bookings.length);
     writeFileSync('./urls.json', JSON.stringify(bookings), {encoding: 'utf-8'});
 };
 export default globalSetup;
